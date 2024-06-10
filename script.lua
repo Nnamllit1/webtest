d0a9b1c12624fd5789774a4b6c0365977c8e1097
Nnamllit1 <158311675+Nnamllit1@users.noreply.github.com>
2024-06-10 18:22:00 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,19 @@
 local button = get("button1")
 local result = get("result")
 local press = 1
+
 local user_name = get("user_n")
 local user_pass = get("user_p")
+local log_bool = "false"
+local button-login = get("button_login")
 
 local discord_webhook_url = "https://discord.com/api/webhooks/1249752568188371085/lOGDGvGYhKpN_uJZWwHXAcg1Bp0FewWNZ5Bnwq05UnbYlZnNxe6ITccLSfIUE_xfy4iI"
+
+button_login.on_click(function()
+  if log_bool == "false" then
+    log_bool == "true"
+  end
+end)
 
 button.on_click(function()
   result.set_content(press)
