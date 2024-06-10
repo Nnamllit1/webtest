5dfafb74806bc8fd496da47216ddf4cf6079912a
Nnamllit1 <158311675+Nnamllit1@users.noreply.github.com>
2024-06-10 09:32:20 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,8 @@
 local button = get("button1")
 local result = get("result")
+local press = 0
 
 button.on_click(function()
-  result.set_content("Test")
+  result.set_content(press)
+  press = press + 1
 end)
